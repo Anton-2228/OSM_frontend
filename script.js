7b9ab705724a0bd1590ce346349e42b0b09dd4d1
anton <anton@zinchenko.pro>
2024-06-18 22:02:22 +0300

    Finished at the front

--- script.js
+++ script.js
@@ -21,7 +21,10 @@ var mymap = L.map('mapid', {
 
 var poly;
 
-export var customControl = L.Control.extend({
+var width = 1;
+var height = Math.sqrt(2)
+
+var SizePolygonControl = L.Control.extend({
     options: {
         position: 'topleft'
     },
@@ -31,33 +34,47 @@ export var customControl = L.Control.extend({
         var firstSizeButton = L.DomUtil.create('button', '', container);
         firstSizeButton.innerHTML = '1 фигура';
         L.DomEvent.on(firstSizeButton, 'click', function() {
-            let height = 0.1;
-            let width = 0.05;
-            poly = create_polygon(height, width)
+            let k = 10000;
+            poly = create_polygon(height*k, width*k)
         });
 
         var secondSizeButton = L.DomUtil.create('button', '', container);
         secondSizeButton.innerHTML = '2 фигура';
         L.DomEvent.on(secondSizeButton, 'click', function() {
-            let height = 0.2;
-            let width = 0.1;
-            poly = create_polygon(height, width)
+            let k = 50000;
+            poly = create_polygon(height*k, width*k)
         });
 
         var thirdSizeButton = L.DomUtil.create('button', '', container);
         thirdSizeButton.innerHTML = '3 фигура';
         L.DomEvent.on(thirdSizeButton, 'click', function() {
-            let height = 0.3;
-            let width = 0.15;
-            poly = create_polygon(height, width)
+            let k = 100000;
+            poly = create_polygon(height*k, width*k)
         });
 
         var fourthSizeButton = L.DomUtil.create('button', '', container);
         fourthSizeButton.innerHTML = '4 фигура';
         L.DomEvent.on(fourthSizeButton, 'click', function() {
-            let height = 0.4;
-            let width = 0.2;
-            poly = create_polygon(height, width)
+            let k = 200000;
+            poly = create_polygon(height*k, width*k)
+        });
+
+        return container;
+    }
+});
+
+var sendPolygonControl = L.Control.extend({
+    options: {
+        position: 'topleft'
+    },
+    onAdd: function(map) {
+        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom-separate');
+
+        // Кнопка с вашей логикой
+        var SendButton = L.DomUtil.create('button', '', container);
+        SendButton.innerHTML = 'Отправить';
+        L.DomEvent.on(SendButton, 'click', function() {
+            sendPoly();
         });
 
         return container;
@@ -66,11 +83,8 @@ export var customControl = L.Control.extend({
 
 function create_polygon(height, width) {
     remove_polygon(poly);
-    height /= 2;
-    width /= 2;
-    let lat = mymap.getCenter()["lat"];
-    let lon = mymap.getCenter()[["lng"]];
-    poly = new L.Polygon([[lat-height, lon-width], [lat-height, lon+width], [lat+height, lon+width], [lat+height, lon-width]],
+    let coord = get_coord_poly(mymap.getCenter(), height, width)
+    poly = new L.Polygon(coord,
         {
             color: '#810541',
             fillColor: '#D462FF',
@@ -80,12 +94,54 @@ function create_polygon(height, width) {
     return poly
 }
 
+function get_coord_poly(center, height, width) {
+    center = coord_to_pix(center)
+    let point1 = [center[0] - width/2, center[1] - height/2]
+    let point2 = [center[0] + width/2, center[1] - height/2]
+    let point3 = [center[0] + width/2, center[1] + height/2]
+    let point4 = [center[0] - width/2, center[1] + height/2]
+
+    return [pix_to_coord(point1), pix_to_coord(point2), pix_to_coord(point3), pix_to_coord(point4)]
+}
+
+var Req = 6378160
+
+function degToRad(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+function radToDeg(rad) {
+    return rad / (Math.PI / 180);
+}
+
+function coord_to_pix(coord) {
+    let lon = degToRad(coord["lng"])
+    let lat = degToRad(coord["lat"])
+    let x = Req * lon
+    let y = Req * Math.log(Math.tan(Math.PI/4 + lat/2))
+    return [x,y]
+}
+
+function pix_to_coord(pix) {
+    let lon = pix[0]/Req
+    let lat = 2 * Math.atan(Math.exp(pix[1]/Req)) - Math.PI/2
+    console.log(pix)
+    console.log(lon)
+    console.log(lat)
+    return [radToDeg(lat), radToDeg(lon)]
+}
+
 function remove_polygon(poly) {
     if (poly != null) {
         poly.remove();
     }
 }
 
+function sendPoly() {
+
+}
+
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
 
-mymap.addControl(new customControl());
+mymap.addControl(new SizePolygonControl());
+mymap.addControl(new sendPolygonControl());
