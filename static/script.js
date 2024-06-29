d2f7cfdf4ec7a06ada99dc625e4b844f3649d3a7
anton <anton@zinchenko.pro>
2024-06-29 13:52:18 +0300

    Added slider and smartphone support

--- static/script.js
+++ static/script.js
@@ -1,9 +1,71 @@
-import 'https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.js';
+// import 'https://unpkg.com/leaflet@1.9.4/dist/leaflet-src.js';
+import 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
 import 'https://unpkg.com/leaflet-contextmenu@1.4.0/dist/leaflet.contextmenu.js';
-import 'https://unpkg.com/leaflet-path-drag@1.9.5/dist/index.js';
+// import 'https://unpkg.com/leaflet-path-drag@1.9.5/dist/index.js';
+// import './test_drag.js'
+// import "https://unpkg.com/leaflet-truesize";
+// import "https://unpkg.com/leaflet-truesize@3.1.0/dist/Leaflet.TrueSize.umd.js";
+// import './leaflet/truesize.js'
 
 import {centerMap, zoomIn, zoomOut} from './context_menu.js'
-import {coord_to_pix, pix_to_coord} from './utils.js'
+import * as utils from './utils.js'
+
+(function() {
+    L.Map.mergeOptions({
+        touchExtend: true
+    });
+
+    L.Map.TouchExtend = L.Handler.extend({
+        initialize: function(map) {
+            this._map = map;
+            this._container = map._container;
+            return this._pane = map._panes.overlayPane;
+        },
+        addHooks: function() {
+            L.DomEvent.on(this._container, 'touchstart', this._onTouchStart, this);
+            L.DomEvent.on(this._container, 'touchend', this._onTouchEnd, this);
+            return L.DomEvent.on(this._container, 'touchmove', this._onTouchMove, this);
+        },
+        removeHooks: function() {
+            L.DomEvent.off(this._container, 'touchstart', this._onTouchStart);
+            L.DomEvent.off(this._container, 'touchend', this._onTouchEnd);
+            return L.DomEvent.off(this._container, 'touchmove', this._onTouchMove);
+        },
+        _onTouchEvent: function(e, type) {
+            var containerPoint, latlng, layerPoint, touch;
+            if (!this._map._loaded) {
+                return;
+            }
+            touch = e.touches[0];
+            containerPoint = L.point(touch.clientX, touch.clientY);
+            layerPoint = this._map.containerPointToLayerPoint(containerPoint);
+            latlng = this._map.layerPointToLatLng(layerPoint);
+            return this._map.fire(type, {
+                latlng: latlng,
+                layerPoint: layerPoint,
+                containerPoint: containerPoint,
+                originalEvent: e
+            });
+        },
+        _onTouchStart: function(e) {
+            return this._onTouchEvent(e, 'touchstart');
+        },
+        _onTouchEnd: function(e) {
+            if (!this._map._loaded) {
+                return;
+            }
+            return this._map.fire('touchend', {
+                originalEvent: e
+            });
+        },
+        _onTouchMove: function(e) {
+            return this._onTouchEvent(e, 'touchmove');
+        }
+    });
+
+    L.Map.addInitHook('addHandler', 'touchExtend', L.Map.TouchExtend);
+
+}).call(this);
 
 var mymap = L.map('mapid', {
     contextmenu: true,
@@ -20,10 +82,32 @@ var mymap = L.map('mapid', {
     }]
 }).setView([55.7522, 37.6156], 13);
 
+mymap.dragging.disable();
+
 var poly;
 
 var width = 1;
-var height = Math.sqrt(2)
+var height = Math.sqrt(2);
+
+var TeleportPolygonControl = L.Control.extend({
+    options: {
+        position: 'topleft'
+    },
+    onAdd: function(map) {
+        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+
+        var teleportButton = L.DomUtil.create('button', '', container);
+        teleportButton.innerHTML = "Переместить область к вам";
+        L.DomEvent.on(teleportButton, 'click', function() {
+              teleport_polygon();
+        });
+
+        L.DomEvent.disableClickPropagation(container);
+        L.DomEvent.disableScrollPropagation(container);
+
+        return container;
+    }
+});
 
 var SizePolygonControl = L.Control.extend({
     options: {
@@ -32,33 +116,22 @@ var SizePolygonControl = L.Control.extend({
     onAdd: function(map) {
         var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
 
-        var firstSizeButton = L.DomUtil.create('button', '', container);
-        firstSizeButton.innerHTML = '1 фигура';
-        L.DomEvent.on(firstSizeButton, 'click', function() {
-            let k = 10000;
-            poly = create_polygon(height*k, width*k)
+        var slider = L.DomUtil.create('input', '', container);
+        slider.type = 'range';
+        slider.min = '1000';
+        slider.max = '80000';
+        slider.value = '10000';
+        slider.step = '1';
+
+        poly = create_polygon(mymap.getCenter(), height * slider.value, width * slider.value);
+
+        L.DomEvent.on(slider, 'input', function() {
+            let k = slider.value;
+            poly = create_polygon(poly.getCenter(), height*k, width*k);
         });
 
-        var secondSizeButton = L.DomUtil.create('button', '', container);
-        secondSizeButton.innerHTML = '2 фигура';
-        L.DomEvent.on(secondSizeButton, 'click', function() {
-            let k = 50000;
-            poly = create_polygon(height*k, width*k)
-        });
-
-        var thirdSizeButton = L.DomUtil.create('button', '', container);
-        thirdSizeButton.innerHTML = '3 фигура';
-        L.DomEvent.on(thirdSizeButton, 'click', function() {
-            let k = 100000;
-            poly = create_polygon(height*k, width*k)
-        });
-
-        var fourthSizeButton = L.DomUtil.create('button', '', container);
-        fourthSizeButton.innerHTML = '4 фигура';
-        L.DomEvent.on(fourthSizeButton, 'click', function() {
-            let k = 200000;
-            poly = create_polygon(height*k, width*k)
-        });
+        L.DomEvent.disableClickPropagation(container);
+        L.DomEvent.disableScrollPropagation(container);
 
         return container;
     }
@@ -85,28 +158,44 @@ var sendPolygonControl = L.Control.extend({
     }
 });
 
-function create_polygon(height, width) {
+function teleport_polygon() {
+    let tl = {lat: poly.getBounds().getNorthWest()["lat"],
+                        lng: poly.getBounds().getNorthWest()["lng"]};
+    let dr = {lat: poly.getBounds().getSouthEast()["lat"],
+        lng: poly.getBounds().getSouthEast()["lng"]};
+
+    tl = utils.coord_to_pix(tl);
+    dr = utils.coord_to_pix(dr);
+
+    let height = tl[1] - dr[1];
+    let width = dr[0] - tl[0];
+
+    poly = create_polygon(mymap.getCenter(), height, width);
+    return poly
+}
+
+function create_polygon(center, height, width) {
     remove_polygon(poly);
-    let coord = get_coord_poly(mymap.getCenter(), height, width)
-    poly = new L.Polygon(coord,
+    let coord = get_coord_poly(center, height, width)
+    poly = new L.Rectangle(coord,
         {
             color: '#810541',
             fillColor: '#D462FF',
-            fillOpacity: 0.5,
-            draggable: true
-        }).addTo(mymap)
+            fillOpacity: 0.5
+        }).addTo(mymap);
+
+    utils.set_on_events(poly, mymap);
     return poly
 }
 
 function get_coord_poly(center, height, width) {
-    console.log(center);
-    center = coord_to_pix(center)
+    center = utils.coord_to_pix(center)
     let point1 = [center[0] - width/2, center[1] - height/2]
     let point2 = [center[0] + width/2, center[1] - height/2]
     let point3 = [center[0] + width/2, center[1] + height/2]
     let point4 = [center[0] - width/2, center[1] + height/2]
 
-    return [pix_to_coord(point1), pix_to_coord(point2), pix_to_coord(point3), pix_to_coord(point4)]
+    return [utils.pix_to_coord(point1), utils.pix_to_coord(point2), utils.pix_to_coord(point3), utils.pix_to_coord(point4)]
 }
 
 function remove_polygon(poly) {
@@ -143,7 +232,6 @@ function sendPoly() {
 }
 
 function check_send_poly_response(data) {
-    console.log(data)
     if (data["status"] === "outdated_page") {
         alert("Оййййй, кажется страничка устарела, cry about it")
     }
@@ -154,5 +242,6 @@ function check_send_poly_response(data) {
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
 
+mymap.addControl(new TeleportPolygonControl());
 mymap.addControl(new SizePolygonControl());
 mymap.addControl(new sendPolygonControl());
--- static/utils.js
+++ static/utils.js
@@ -21,3 +21,80 @@ export function pix_to_coord(pix) {
     let lat = 2 * Math.atan(Math.exp(pix[1]/Req)) - Math.PI/2
     return [radToDeg(lat), radToDeg(lon)]
 }
+
+function isPointInRectangle(latlng, rectangle) {
+    let bounds = rectangle.getBounds();
+    return bounds.contains(latlng);
+}
+
+export function set_on_events(poly, mymap) {
+    var isDragging = false;
+    var startLatLng = 0;
+    mymap.on('touchstart', function(e) {
+        let latlng = e.latlng;
+        if (isPointInRectangle(latlng, poly)) {
+            isDragging = true;
+            startLatLng = latlng;
+            mymap.dragging.disable();
+        }
+    });
+
+    mymap.on('touchmove', function(e) {
+        if (isDragging) {
+            // var latlng = e.touches ? map.mouseEventToLatLng(e.touches[0]) : e.latlng;
+            let latlng = e.latlng;
+            let offset = L.latLng(
+                latlng.lat - startLatLng.lat,
+                latlng.lng - startLatLng.lng
+            );
+            let b = poly.getBounds();
+
+            let newBounds = [
+                [b.getSouthWest()["lat"] + offset.lat, b.getSouthWest()["lng"] + offset.lng],
+                [b.getNorthEast()["lat"] + offset.lat, b.getNorthEast()["lng"] + offset.lng]
+            ];
+
+            poly.setBounds(newBounds);
+            startLatLng = latlng;
+        }
+    });
+
+    mymap.on('touchend', function() {
+        if (isDragging) {
+            isDragging = false;
+            mymap.dragging.enable();
+        }
+    });
+
+    poly.on('mousedown', function(e) {
+        isDragging = true;
+        startLatLng = e.latlng;
+        mymap.dragging.disable();
+    });
+
+    mymap.on('mousemove', function(e) {
+        if (isDragging) {
+            let latlng = e.latlng;
+            let offset = L.latLng(
+                latlng.lat - startLatLng.lat,
+                latlng.lng - startLatLng.lng
+            );
+
+            let b = poly.getBounds();
+            let newBounds = [
+                [b.getSouthWest()["lat"] + offset.lat, b.getSouthWest()["lng"] + offset.lng],
+                [b.getNorthEast()["lat"] + offset.lat, b.getNorthEast()["lng"] + offset.lng]
+            ];
+
+            poly.setBounds(newBounds);
+            startLatLng = latlng;
+        }
+    });
+
+    mymap.on('mouseup', function() {
+        if (isDragging) {
+            isDragging = false;
+            mymap.dragging.enable();
+        }
+    });
+}
